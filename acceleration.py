588b9ebe260e5e87bac3ba803b8c231f6cb42b97
Thaenthai <thaenthai124@gmail.com>
2018-07-26 00:48:59 +0700

    Simple Acceleration Calculator

--- acceleration.py
+++ acceleration.py
@@ -1,11 +1,11 @@
+#for simple calculate only
 def simple_acceleration_caculator(initial_v, final_v, time):
     delta_v = final_v - initial_v
-    return delta_v
     acceleration = delta_v / time
     return acceleration
+#to get information
 initial_v = input("initial v(m/s) : ") 
 final_v = input("final v(m/s) : ")
 time = input("time(s) : ")
 
 print simple_acceleration_caculator(initial_v, final_v, time) , "metre per second square"
-print "Oh yes"
