6f1945850b6f04c0ce43705ce9ca392b3f73a5cb
NeahtX <thaenthai124@gmail.com>
2022-11-28 00:28:50 +0700

    Add check_factorial function

--- function/factorial.py
+++ function/factorial.py
@@ -3,6 +3,19 @@ def factorial(number):
         return number
     return number * factorial(number - 1)
 
-print(factorial(5))
+def check_factorial(number):
+    ans = 1
+    for i in range(number-1):
+        ans *= number - i
+    return ans
 
-print(factorial(100))
+
+
+print(f'This is from factorial function \n {factorial(5)}')
+print(f'This is from check_factorial function \n {check_factorial(5)}')
+
+print(f'This is from factorial function \n {factorial(20)}')
+print(f'This is from check_factorial function \n {check_factorial(20)}')
+
+print(f'This is from factorial function \n {factorial(100)}')
+print(f'This is from check_factorial function \n {check_factorial(100)}')
