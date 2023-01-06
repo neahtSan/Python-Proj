2e3a6acfb3509cf8fd842bd6192bc4d70856c315
Thaenthai <thaenthai124@gmail.com>
2023-01-06 23:43:29 +0700

    Add granades

--- python-script/apexRand/data.py
+++ python-script/apexRand/data.py
@@ -1,3 +1,4 @@
 characters = ['bh', 'wr', 'bl']
 guns = ['r301', 'flatline', 'r99']
 items = {'health':['med','syn'], 'shield':['cell', 'bat'], 'survivor':['heat-shield', 'respawn-beacon']}
+granades = ['frag', 'arc-star', 'thermite']
