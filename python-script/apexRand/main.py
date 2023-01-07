fa0155a37690f18213d8a1d3784db29589d3a438
Thaenthai <thaenthai124@gmail.com>
2023-01-07 23:27:51 +0700

    Add prototype Party function

--- python-script/apexRand/main.py
+++ python-script/apexRand/main.py
@@ -1,2 +1,15 @@
 from data import *
-print(characters)
+import random
+
+def Party(sqaud_member):
+    party = dict()
+    if type(sqaud_member) == int:
+        if sqaud_member >= 0 and sqaud_member <= 3:
+            for i in range(sqaud_member):
+                party[f'member{i+1}'] = random.choice(characters)
+    return party
+
+print(Party(0))
+print(Party(1))
+print(Party(2))
+print(Party(3))
