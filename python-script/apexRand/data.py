5d2d47c881569629b9461b742ffd22841f36f4a1
Thaenthai <thaenthai124@gmail.com>
2023-01-07 23:27:26 +0700

    Add armors

--- python-script/apexRand/data.py
+++ python-script/apexRand/data.py
@@ -2,3 +2,4 @@ characters = ['bh', 'wr', 'bl']
 guns = ['r301', 'flatline', 'r99']
 items = {'health':['med','syn'], 'shield':['cell', 'bat'], 'survivor':['heat-shield', 'respawn-beacon']}
 granades = ['frag', 'arc-star', 'thermite']
+armors = ['white', 'blue', 'purple', 'red']
