817fbbef7cdd10de5cf716d569c4dd097fdd3533
NeahtX <thaenthai124@gmail.com>
2023-01-14 17:05:16 +0700

    Improve random member logic

--- python-script/apexRand/main.py
+++ python-script/apexRand/main.py
@@ -5,8 +5,10 @@ def Party(sqaud_member):
     party = dict()
     if type(sqaud_member) == int:
         if sqaud_member >= 0 and sqaud_member <= 3:
+            member = characters
             for i in range(sqaud_member):
-                party[f'member{i+1}'] = random.choice(characters)
+                party[f'member{i+1}'] = random.choice(member)
+                member.remove(party[f'member{i+1}'])
     return party
 
 print(Party(0))
