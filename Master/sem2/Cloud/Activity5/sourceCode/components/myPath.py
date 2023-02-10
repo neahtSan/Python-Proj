842f226bfcf3a3ad11bbb9fc1084c5dd508a52da
Thaenthai <thaenthai124@gmail.com>
2023-02-11 02:35:30 +0700

    Add get upload directory path and all file in directory function

--- Master/sem2/Cloud/Activity5/sourceCode/components/myPath.py
+++ Master/sem2/Cloud/Activity5/sourceCode/components/myPath.py
@@ -12,3 +12,30 @@ def download_path():
     
     return save_directory
 
+def upload_path():
+    directory = pathlib.Path(__file__).parent.resolve()
+    string_directory = str(directory)
+    list_directory = string_directory.split('\\')
+    save_directory = '\\'.join(list_directory[0:-1]+['upload'])
+    
+    if not os.path.isdir(save_directory):
+        os.mkdir(save_directory)
+    
+    return save_directory
+
+def all_upload_file():
+    # folder path
+    dir_path = upload_path()
+
+    # list to store files
+    allFile = []
+
+    # Iterate directory
+    for path in os.listdir(dir_path):
+        # check if current path is a file
+        if os.path.isfile(os.path.join(dir_path, path)):
+            allFile.append(path)
+            
+    return allFile
+    
+
