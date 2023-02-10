723c3ac42ce02a7db75a79afb3da4909773f8db8
Thaenthai <thaenthai124@gmail.com>
2023-02-11 02:35:03 +0700

    Add put & get function

--- Master/sem2/Cloud/Activity5/sourceCode/components/logic.py
+++ Master/sem2/Cloud/Activity5/sourceCode/components/logic.py
@@ -2,7 +2,8 @@ import requests
 import json
 import base64
 import os
-from myPath import *
+from components.myPath import *
+from components.check import *
 
 gateway = ""
 payload={}
@@ -17,22 +18,46 @@ def get(fileName):
     path = "/Activity-5-download" 
     url = f'{gateway}{path}'
     payload = json.dumps({
-    "file": "mika.jpg"
+    "fileName": f'{fileName}'
     })
     
-    file = requests.request("GET", url, headers=headers, data=payload)
-    text = file.text
-    base64_message = bytes(text, "utf-8")
+    file = requests.request("POST", url, headers=headers, data=payload)
+    
+    if file.status_code != 200:
+        return print(file.text)
+    
+    rawText = file.text
+    base64_message = bytes(rawText, "utf-8")
     
     save_directory = download_path()
     
-    name = 'test1.txt'
-    file_path = os.path.join(save_directory, name)
+    file_path = os.path.join(save_directory, fileName)
     
     with open(file_path, "wb") as fh:
         fh.write(base64.decodebytes(base64_message))
     
-    return print(save_directory)
+    return print(f'download {fileName} at {save_directory} complete!')
+
+def put(fileName):
+    path = "/Activity-5-upload"
+    url = f'{gateway}{path}'
+    
+    if check_file(fileName) != True:
+        return print(f'no file name: {fileName} in {upload_path()}')
+    
+    upload_directory = upload_path()
+    byte = open(os.path.join(upload_directory, fileName), "rb").read(1024)
+    encode = base64.b64encode(byte)
+    body = encode.decode('ascii')
+    
+    payload = json.dumps({
+        "fileName": f'{fileName}',
+        "rawText": json.loads(json.dumps(body, default=str))
+    })
+    
+    file = requests.request("POST", url, headers=headers, data=payload)
+    
+    return print(file.text)
 
 
 #url = 'http://google.com/favicon.ico'
