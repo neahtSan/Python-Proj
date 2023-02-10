583e9b9d0168bfd0bc79e1c53148ffd7d01dc57d
Thaenthai <thaenthai124@gmail.com>
2023-02-11 02:34:07 +0700

    Add check upload file

--- Master/sem2/Cloud/Activity5/sourceCode/components/check.py
+++ Master/sem2/Cloud/Activity5/sourceCode/components/check.py
@@ -1,7 +1,13 @@
+from components.myPath import *
 
 def check_input(input):
     if input.isnumeric():
         return f'input value cannot be numeric'
-    if input.split()[0] not in ['view', 'get', 'put', 'quit']:
+    if input.split()[0] not in ['help', 'view', 'get', 'put', 'quit']:
         return f'Wrong input order'
     return True
+
+def check_file(fileName):
+    if fileName in all_upload_file():
+        return True
+    return False
