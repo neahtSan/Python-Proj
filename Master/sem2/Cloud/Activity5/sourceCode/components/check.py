add028099fa72b6eea88328eed5d312c04e61fdf
Thaenthai <thaenthai124@gmail.com>
2023-02-10 22:10:52 +0700

    Add check input

--- Master/sem2/Cloud/Activity5/sourceCode/components/check.py
+++ Master/sem2/Cloud/Activity5/sourceCode/components/check.py
@@ -2,6 +2,6 @@
 def check_input(input):
     if input.isnumeric():
         return f'input value cannot be numeric'
-    if input.split()[0] not in ['view', 'make', 'quit']:
+    if input.split()[0] not in ['view', 'get', 'put', 'quit']:
         return f'Wrong input order'
     return True
