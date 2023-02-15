0f97670a93d0ed6ce730e5af9ca125562b867972
NeahtX <thaenthai124@gmail.com>
2023-02-15 16:38:19 +0700

    Handle EOFError

--- Master/sem2/Cloud/Activity5/sourceCode/myDropBox.py
+++ Master/sem2/Cloud/Activity5/sourceCode/myDropBox.py
@@ -7,37 +7,40 @@ greet()
 
 notQuit = True
 while notQuit:
-    #get input from user
-    rawInput = input(">> ")
-    command = str()
-    
-    #check input
-    if check_input(rawInput) != True:
-        print(check_input(rawInput))
+    try:
+        #get input from user
+        rawInput = input(">> ")
+        command = str()
         
-    #help manual
-    if rawInput == "help":
-        help()
+        #check input
+        if check_input(rawInput) != True:
+            print(check_input(rawInput))
+            
+        #help manual
+        if rawInput == "help":
+            help()
 
-    #exit
-    if rawInput == "quit":
-        notQuit = False
-        
-    #handle put & get command
-    if rawInput != '' and rawInput.split()[0] in ['put', 'get'] and len(rawInput.split()) > 1:
-        command, fileName  = rawInput.split()
+        #exit
+        if rawInput == "quit":
+            notQuit = False
+            
+        #handle put & get command
+        if rawInput != '' and rawInput.split()[0] in ['put', 'get'] and len(rawInput.split()) > 1:
+            command, fileName  = rawInput.split()
 
-    #view
-    if rawInput == "view":
-        view()
-        
-    #download
-    if command == "get":
-        get(fileName)
-        
-    #upload
-    if command == "put":
-        put(fileName)
+        #view
+        if rawInput == "view":
+            view()
+            
+        #download
+        if command == "get":
+            get(fileName)
+            
+        #upload
+        if command == "put":
+            put(fileName)
+    except EOFError as e:
+       notQuit = False
         
     
 
